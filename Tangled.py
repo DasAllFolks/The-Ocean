aaf13cc2394ea2f5259b93e02f27bd22f9f2dbca
NiktheCurious <nikki.m.hsu@gmail.com>
2015-05-14 21:53:49 -0600

    First Commit

---
+++ Tangled.py
@@ -0,0 +1,8 @@
+print "In one word, what is the hardest thing about life right now?",
+hardest = raw_input()
+print "In one word, what is the most beautiful thing about life right now?",
+beautiful = raw_input()
+print "In one word, what is the biggest thing you've overcome?",
+overcome = raw_input()
+
+print "Don't be so afraid of %r, just focus on your %r. You've survived %r, and things will only continue to get better!" % (hardest, beautiful, overcome)
